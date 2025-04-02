447d5c76ba14f7098d223556237282755da0fa61
harshivpatel <hp200378@gmail.com>
2025-04-02 13:19:19 +0100

    Implement NewsAPI in the backend

--- frontend/src/app/service/news.service.spec.ts
+++ frontend/src/app/service/news.service.spec.ts
@@ -1,4 +1,4 @@
-import { Injectable  } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -6,12 +6,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class NewsService {
-  private apiURL = 'https://newsapi.org/v2/top-headlines';
-  private apiKey = '241d9cda125a47f5a816a7fc8fa1cc0a';
+  private backendUrl = 'http://localhost:5000/api/news';
 
   constructor(private http: HttpClient) {}
 
-  getNews(country: String = 'ie'): Observable<any> {
-    return this.http.get<any>(`${this.apiURL}?country=${country}&apiKey=${this.apiKey}`);
+  getNews(): Observable<any> {
+    return this.http.get<any>(this.backendUrl);
   }
 }
