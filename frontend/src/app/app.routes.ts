db87e8202c3d934c70468c3add5118c05546450b
harshivpatel <hp200378@gmail.com>
2025-04-17 12:19:34 +0100

    Add about us page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
-import { MyAccountComponent } from './components/my-account/my-account.component'; // <- new import
+import { MyAccountComponent } from './components/my-account/my-account.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: SignInComponent },
-  { path: 'my-account', component: MyAccountComponent }
+  { path: 'my-account', component: MyAccountComponent },
+  { path: 'about-us', component: AboutUsComponent }
 ];
