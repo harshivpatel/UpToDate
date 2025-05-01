95f8130002db2cbe1e5067b3bb28589448ca03c8
harshivpatel <hp200378@gmail.com>
2025-05-01 13:24:55 +0100

    Fix html issues in right sidebar pages

--- frontend/src/app/components/about-us/about-us.component.ts
+++ frontend/src/app/components/about-us/about-us.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [CommonModule, SidebarComponent],
+  imports: [CommonModule, SidebarComponent, RouterModule],
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
